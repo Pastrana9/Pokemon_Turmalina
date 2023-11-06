3c080af3ff5d461b5ee7df0e35ee1bef2bd7dadb
AlvaroPereda <121577598+AlvaroPereda@users.noreply.github.com>
2023-11-06 23:35:07 +0100

    Add files via upload
    Segunda versión del Pokemon Turmalina

---
+++ Pokemon Turmalina/src/application/ControllerAjustes.java
@@ -0,0 +1,47 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class ControllerAjustes {
+
+	
+	@FXML
+    private AnchorPane ajustesAnchorPane;
+	
+    @FXML
+    private void ButtonVolver(ActionEvent event) {
+        Stage pantallaAjustes = (Stage) ajustesAnchorPane.getScene().getWindow();
+        pantallaAjustes.close();
+    }
+
+    @FXML
+    private void ButtonTitulo(ActionEvent event)
+    {
+        try {
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("PantallaInicio.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+
+            Stage Inicio = new Stage();
+
+            Inicio.setTitle("Videojuego");
+            Inicio.setScene(scene);
+
+            Inicio.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+}
---
+++ Pokemon Turmalina/src/application/ControllerInformacion.java
@@ -0,0 +1,18 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ControllerInformacion {
+
+    @FXML
+    private void ButtonVolver(ActionEvent event) {
+
+    }
+
+
+}
