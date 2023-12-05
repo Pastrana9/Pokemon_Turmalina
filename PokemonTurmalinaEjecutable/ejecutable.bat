5170108789744bcf9109dc9e5aa608fe23c9c833
AlvaroPereda <121577598+AlvaroPereda@users.noreply.github.com>
2023-12-05 16:39:38 +0100

    ejecutable

---
+++ PokemonTurmalinaEjecutable/ejecutable.bat
@@ -0,0 +1,2 @@
+@echo off
+java --module-path C:\Users\...\PokemonTurmalinaEjecutable\javafx-sdk-21.0.1\lib --add-modules=javafx.controls,javafx.fxml -jar "C:\Users\...\PokemonTurmalinaEjecutable\PokemonTurmalina.jar"
